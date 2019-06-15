ff04976bea638bb60540a14ab36c7462066107c2
Alex <alex.katorzhin@gmail.ru>
2019-06-15 13:35:12 +0300

    class HuffmanCode

--- src/module2/HuffmanCode.java
+++ src/module2/HuffmanCode.java
@@ -1,4 +1,22 @@
 package module2;
 
+import java.util.PriorityQueue;
+
 public class HuffmanCode {
+    public HuffmanTree buildTree(int[] charFreqs) {
+        PriorityQueue<HuffmanTree> trees = new PriorityQueue<>();
+        for (int i = 0; i < charFreqs.length; i++) {
+            if (charFreqs[i] > 0) {
+                trees.offer(new HuffmanLeaf(charFreqs[i], (char) i));
+            }
+        }
+        assert trees.size() > 0;
+        while (trees.size() > 1) {
+            HuffmanTree a = trees.poll();
+            HuffmanTree b = trees.poll();
+
+            trees.offer(new HuffmanNode(a, b));
+        }
+        return trees.poll();
+    }
 }
