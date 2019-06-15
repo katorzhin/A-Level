50a710ed142570c77c8a3b702698bd9b2c789ef2
Alex <alex.katorzhin@gmail.ru>
2019-06-15 11:54:37 +0300

    class HuffmanTree

--- src/module2/HuffmanTree.java
+++ src/module2/HuffmanTree.java
@@ -1,4 +1,14 @@
 package module2;
 
-public class HuffmanTree {
+public class HuffmanTree implements Comparable<HuffmanTree> {
+    public final int frequency;
+
+    public HuffmanTree(int freq) {
+        frequency = freq;
+    }
+
+    @Override
+    public int compareTo(HuffmanTree o) {
+        return 0;
+    }
 }
