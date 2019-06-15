ccad185ee1fd5cf878961ff437091000632ecc18
Alex Katorzhin <46688550+katorzhin@users.noreply.github.com>
2019-06-15 13:12:26 +0300

    create class HuffmanLeaf

--- src/module2/HuffmanTree.java
+++ src/module2/HuffmanTree.java
@@ -4,11 +4,11 @@ public class HuffmanTree implements Comparable<HuffmanTree> {
     public final int frequency;
 
     public HuffmanTree(int freq) {
-        frequency = freq;
+        this.frequency = freq;
     }
 
     @Override
-    public int compareTo(HuffmanTree o) {
-        return 0;
+    public int compareTo(HuffmanTree tree) {
+        return frequency - tree.frequency;
     }
 }
