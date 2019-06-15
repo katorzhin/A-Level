a57f2b831d19a10dfdcdc877bf5732d4613d524c
Alex <alex.katorzhin@gmail.ru>
2019-06-15 14:03:35 +0300

    class HuffmanNode

--- src/module2/HuffmanNode.java
+++ src/module2/HuffmanNode.java
@@ -1,4 +1,11 @@
 package module2;
 
 public class HuffmanNode {
+    public final HuffmanTree left,right;
+
+    public HuffmanNode(HuffmanTree l,HuffmanTree r){
+        super(l.frequency+r.frequency);
+        left=l;
+        right=r;
+    }
 }
