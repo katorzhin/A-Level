24006be13117d6b20122f77bfec7781a24995b01
Alex <alex.katorzhin@gmail.ru>
2019-06-15 13:58:08 +0300

    class Main

--- src/module2/Main.java
+++ src/module2/Main.java
@@ -1,4 +1,16 @@
 package module2;
 
 public class Main {
+    public static void main(String[] args) {
+        String test = "Hello World";
+
+        HuffmanCode newCode = new HuffmanCode();
+
+        int[] charFreqs = new int[256];
+        for (char c : test.toCharArray()) {
+            charFreqs[c]++;
+        }
+        HuffmanTree tree = newCode.buildTree(charFreqs);
+
+    }
 }
